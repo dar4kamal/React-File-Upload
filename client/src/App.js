d78b49160ffe4e72b2138446c31af46de624e905
dar4kamal <sasaa9646@gmail.com>
2020-10-06 01:59:14 +0200

    added Home Page Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./App.css";
+import FileUpload from "./components/FileUpload";
 
-const App = () => <div className="App">app</div>;
+const App = () => (
+	<div className="container mt-4">
+		<h4 className="display-4 text-center mt-4">
+			<i className="fab fa-react">React File Upload</i>
+			<i className="fab fa-react" />
+		</h4>
+		<FileUpload />
+	</div>
+);
 
 export default App;
